c11445043289bcd0031225dd7d5e00be47bab378
Zack Stickles <zstickles@newrelic.com>
2020-04-12 15:43:08 -0700

    Cleanup

--- src/elements/forest.js
+++ src/elements/forest.js
@@ -1,39 +1,13 @@
 import { range, rand } from '../utils/helpers';
 import { point, isPointInPoly } from '../utils/math';
-import { drawCircle } from '../utils/art';
 import randomPoly from '../utils/randPoly';
 import tree from './tree';
-
-// const pointCircle = ({ paper, pos, size }) => {
-//   const numTrees = size; // density
-//   return range(numTrees).map((i) => {
-//     const a = Math.random() * 2 * Math.PI;
-//     const r = size * Math.sqrt(Math.random());
-//     const x = r * Math.cos(a) + pos.x;
-//     const y = r * Math.sin(a) + pos.y;
-//     return point(x, y);
-//   });
-// };
 
 const forest = ({
   paper,
   pos,
   size = 100
 }) => {
-  // const points = range(rand(2, 4)).reduce((acc) => {
-  //   const x = rand(pos.x - size, pos.x + size);
-  //   const y = rand(pos.y - size, pos.y + size);
-  //   const center = point(x, y);
-  //   return [
-  //     ...acc,
-  //     ...pointCircle({ paper, pos: center, size })
-  //   ];
-  // }, []);
-
-  // points
-  //   .sort((a, b) => a.y < b.y ? -1 : 1)
-  //   .forEach(pos => tree({ paper, pos }));
-
   const treeSize = 10;
 
   // get random shape
